47ef3dc4d0893a3d23b6ee06834758da6ed781e7
T. Lanette <TLPollard4516@gmail.com>
2020-08-11 15:20:03 -0400

    Updates
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

---
+++ scripts/main.js
@@ -0,0 +1,5 @@
+//Mobile Navigation 
+const hamburger = document.getElementById('hamburger');
+const close = document.getElementById('close');
+const mobileNav = document.getElementById('mobileNav');
+
