5a23a0b4c146ae25a11419935d11633d24537e20
T. Lanette <TLPollard4516@gmail.com>
2020-08-11 20:01:54 -0400

    Updates
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,26 @@
 //Mobile Navigation 
+const hamburger = document.getElementById('hamburger');
+const close = document.getElementById('close');
+const nav = document.getElementById('nav-menu');
+
+hamburger.addEventListener('click', () => {
+    hamburger.style.display = 'none';
+    close.style.display = 'block';
+    nav.classList.add('mobile-nav');
+})
+
+close.addEventListener('click', () => {
+    close.style.dislay = 'none';
+    hamburger.style.display = 'block';
+    nav.classList.remove('mobile-nav');
+})
+
+window.addEventListener('resize', () => {
+    close.style.display = 'none';
+    nav.classList.remove('mobile-nav')
+    if (window.innerWidth > 880) {
+        hamburger.style.display = 'none';
+    } else {
+        hamburger.style.display = 'block';
+    }
+})
